47d899add4e438b54e23f42f26850e0823a06272
netizen-0 <rishabhytpremium17@gmail.com>
2025-08-28 23:02:52 +0530

    Update moderation.py

--- Biolink/plugins/moderation.py
+++ Biolink/plugins/moderation.py
@@ -2,15 +2,28 @@ from pyrogram import filters
 from pyrogram.types import Message
 from bot.utils.database import add_warn, get_warns, mute_user, is_link_or_username
 
+async def is_user_admin(client, chat_id, user_id):
+    member = await client.get_chat_member(chat_id, user_id)
+    return member.status in ["administrator", "creator"]
+
 def register(app):
     @app.on_message(filters.group & ~filters.service)
     async def moderate_message(client, message: Message):
         if message.from_user and message.text:
             text = message.text.lower()
+            
+            # Check for links
             if "http://" in text or "https://" in text or "t.me/" in text:
                 if is_link_or_username(text):
+                    
+                    # Check if user is admin
+                    if await is_user_admin(client, message.chat.id, message.from_user.id):
+                        return  # Do nothing if the user is admin
+
                     await message.delete()
+
                     warns = await add_warn(message.chat.id, message.from_user.id)
+
                     if warns >= 4:
                         await mute_user(client, message.chat.id, message.from_user.id, reason="4 warnings")
                     else:
